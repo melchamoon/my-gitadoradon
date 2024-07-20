20dcd861a3cf46f211ffaf0fc0ad4042a35a01a5
melchamoon <melchamoon@example.com>
2024-07-20 18:23:15 +0900

    データベース名を更新

--- js/firebase.js
+++ js/firebase.js
@@ -8,7 +8,7 @@ const firebaseConfig = {
   appId: "1:1054794233764:web:1056a77a661119aa08b331",
   measurementId: "G-3EE806DGKJ",
 };
-const databaseName = "rankings_20240707";
+const databaseName = "rankings_20240720";
 // Firebaseを初期化
 firebase.initializeApp(firebaseConfig);
 
